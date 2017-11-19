240dd2dcd4935016e8532dc230397a445019a6ef
David <Davidmonjenavidad@gmail.com>
2017-11-19 17:48:50 +0100

    Slogan moviment

--- js/codi.js
+++ js/codi.js
@@ -3,3 +3,22 @@ document.getElementById("buto").onclick = function() {apretar()};
 function apretar() {
     alert("Hola Santi fem tecno? :D");
 }
+
+$('#slogan').typeIt({
+     speed: 50,
+     autoStart: false
+})
+.tiType('Wll')
+.tiPause(500)
+.tiDelete(2)
+.tiType('ell, ')
+.tiPause(1000)
+.tiType('I guess I\'m typing..')
+.tiBreak() .tiPause(750)
+.tiType(' but I don\'t really know what to say')
+.tiSettings({speed: 700})
+.tiType('...')
+.tiPause(750)
+.tiSettings({speed: 50})
+.tiDelete()
+.tiType('IS THAT SO <strong>WRONG??</strong>');
