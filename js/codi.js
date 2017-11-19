05601bc83306fa5e5a748fdfe1da19ade82eaa95
Santi <scros97@gmail.com>
2017-11-19 20:23:19 +0100

    Gradient i countdown

--- js/codi.js
+++ js/codi.js
@@ -17,6 +17,6 @@ $('#slogan').typeIt({
 
 //RELLOTGE COMPTE ENRRERA 
 
-$('#clock').countdown('2020/10/10', function (event) {
-    $(this).html(event.strftime('%D days %H:%M:%S'));
+$('#clock').countdown('2017/11/22', function (event) {
+    $(this).html(event.strftime('%D d %H h %M m %S s'));
 });
