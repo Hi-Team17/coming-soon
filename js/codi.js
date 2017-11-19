0366b33abbe54cf104a112321dfd5d50dccd5419
Santi <scros97@gmail.com>
2017-11-19 19:56:39 +0100

    Format

--- js/codi.js
+++ js/codi.js
@@ -1,22 +1,22 @@
 //LLETRES MOVIMENT
 $('#slogan').typeIt({
-     speed: 140,
-     autoStart: false,
-     loop: true
-})
-.tiType('<strong>H</strong>uman ')
-.tiPause(100)
-.tiType('<strong>I</strong>nterfaces')
-.tiPause(1000)
-.tiDelete(9)
-.tiPause(100)
-.tiType('ntelligence')
-.tiPause(1000)
-.tiDelete(11)
-.tiType('nteractions')
+        speed: 140,
+        autoStart: false,
+        loop: true
+    })
+    .tiType('<strong>H</strong>uman ')
+    .tiPause(100)
+    .tiType('<strong>I</strong>nterfaces')
+    .tiPause(1000)
+    .tiDelete(9)
+    .tiPause(100)
+    .tiType('ntelligence')
+    .tiPause(1000)
+    .tiDelete(11)
+    .tiType('nteractions')
 
 //RELLOTGE COMPTE ENRRERA 
 
-$('#clock').countdown('2020/10/10', function(event) {
-  $(this).html(event.strftime('%D days %H:%M:%S'));
+$('#clock').countdown('2020/10/10', function (event) {
+    $(this).html(event.strftime('%D days %H:%M:%S'));
 });
